dc51d0fc410bbbec4eeb3f44d7584eaa8fd7f8a3
Seth Chaiken <sdc@cs.albany.edu>
2026-03-20 16:43:05 -0400

    Reversion testing ok. Reversion.sh script improved and added.

--- ReversionTest.sh
+++ ReversionTest.sh
@@ -2,23 +2,28 @@
 
 ./C-TAP.sh
 
+echo "Reversion test: C-TAP.sh finished. Diffing results:"
+
+
 NAME="DroneShort1"
 
-if diff OLDRESULTS/$NAME.int RESULTS/$NAME.int
+if diff OLDRESULTS/$NAME.int RESULTS/$NAME.int > RESULTS/$NAME.int.diff
 then
     echo good for $NAME.int
+    rm RESULTS/$NAME.int.diff
 else
-    echo "DIFFERENCE! OLDRESULTS/$NAME.int RESULTS/$NAME.int"
+    echo "DIFFERENCE! OLDRESULTS/$NAME.int RESULTS/$NAME.int" RESULTS/$NAME.int.diff
 fi
 
-if diff OLDRESULTS/$NAME.out RESULTS/$NAME.out
+if diff OLDRESULTS/$NAME.out RESULTS/$NAME.out > RESULTS/$NAME.out.diff
 then
     echo good for $NAME.out
+    rm RESULTS/$NAME.out.diff
 else
     echo "DIFFERENCE! OLDRESULTS/$NAME.out RESULTS/$NAME.out"
 fi
 
-if diff OLDRESULTS/$NAME.MOV RESULTS/$NAME.MOV
+if diff OLDRESULTS/$NAME.MOV RESULTS/$NAME.MOV > /dev/null
 then
     echo good for $NAME.MOV
 else
