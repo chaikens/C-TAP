85279cdd095a55537bf34d3faace036fde300fff
Seth Chaiken <sdc@cs.albany.edu>
2026-04-06 14:10:32 -0400

    Back to pipelining..made very short movie, programmed loop stop in TryPipeline, renamed Try.. experimentation scripts.

--- Trying/TryPipelineMovieToYUVsToBMP.sh
+++ Trying/TryPipelineMovieToYUVsToBMP.sh
@@ -8,8 +8,12 @@ coproc YUVER { ffmpeg -hide_banner -y -i DroneShort1.mp4 PIPE.yuv &> YUVER.messa
 NN=0
 while echo $((NN++))  
 do
-    dd iflag=fullblock bs=2160 count=$((3840*3)) of="DS$NN.yuv" <& ${YUVER[0]}
-    ffmpeg -hide_banner -y -video_size 3840x2160 -i DS$NN.yuv -frames:v 1 DS$NN.bmp
+  if [ "${YUVER[0]}" = "" ]
+    then
+	break
+  fi
+  dd iflag=fullblock bs=2160 count=$((3840*3)) of="DS$NN.yuv" <& ${YUVER[0]}
+  ffmpeg -hide_banner -y -video_size 3840x2160 -i DS$NN.yuv -frames:v 1 DS$NN.bmp
 done
 
 
