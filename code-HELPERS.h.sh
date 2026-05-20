0293d55fd073904d716235e20309b0524d6e4491
Seth Chaiken <sdc@cs.albany.edu>
2026-05-20 15:05:40 -0400

    help with HELPERS

---
+++ code-HELPERS.h.sh
@@ -0,0 +1,115 @@
+echo SOURCED: code-HELPERS.h.sh
+#
+#  Helper shell functions
+#
+
+#Phase1a reads depth 24 (8x8x8) .bmps only
+function depthOfBmpIs24()
+{
+    if [ ! -r $1 ]
+    then
+	return 1
+    else
+        d=$( file $1 | sed 's/.*format, \([0-9]\+\) x \([0-9]\+\) x \([0-9]\+\).*/\3/' )
+        #[sdc trial/err->] substitute/match..widthdigs x heightdigs  x depthdigs  .*/ print depth/
+  	#thumb000001.bmp: PC bitmap, Windows 3.x format, 1920 x 1080 x 24, image size 6220800, cbSize 6220854, bits offset 54
+	if [ $d"" = 24 ]
+	then
+            return 0
+	else
+	    return 1
+        fi
+    fi	
+}
+function widthOfBmp()
+{
+    if [ ! -r $1 ]
+    then
+	return 1
+    else
+        echo $( file $1 | sed 's/.*format, \([0-9]\+\) x \([0-9]\+\) x \([0-9]\+\).*/\1/' )
+        #[sdc trial/err->] substitute/match..widthdigs x heightdigs  x depthdigs  .*/ print width/
+  	#thumb000001.bmp: PC bitmap, Windows 3.x format, 1920 x 1080 x 24, image size 6220800, cbSize 6220854, bits offset 54
+	return 0
+    fi
+}
+function heightOfBmp()
+{
+    if [ ! -r $1 ]
+    then
+	return 1
+    else
+       echo $( file $1 | sed 's/.*format, \([0-9]\+\) x \([0-9]\+\) x \([0-9]\+\).*/\2/' )
+    	return 0
+    fi
+}	 
+function widthOfMovie() {
+    ffprobe -v quiet  -show_streams $1 | sed -n 's/^width=\([1-9][0-9]*\)$/\1/p'
+}
+
+function heightOfMovie() {
+    ffprobe -v quiet  -show_streams $1 | sed -n 's/^height=\([1-9][0-9]*\)$/\1/p'
+    }
+
+function padTo6Digs() {
+    if [ $1 -lt 10 ]
+    then
+        pad="00000"
+    elif [ $1 -lt 100 ]
+    then
+        pad="0000"
+    elif [ $1 -lt 1000 ]
+    then
+        pad="000"
+    elif [ $1 -lt 10000 ]
+    then
+        pad="00"
+    elif [ $1 -lt 100000 ]
+    then
+        pad="0"
+    else
+	if [ !$? ]
+	then
+	    return 1
+	fi
+        pad=""
+    fi
+    echo "${pad}$1"
+}
+
+function exit_greeting() {
+echo "True, full consciousness can now be reasserted by you poor human user."
+
+echo "See the results in the xterms and ${RESULTS_DIR}"
+echo -n "Type yes to kill the xterms. Or, no, or wait 20sec:"
+if ! read -t 20 yes
+then
+    echo OK. Time for asking done, leaving the xterms.
+    echo Kill them manually or use ./killxterms.sh
+fi
+
+if [ ${yes}"" = "yes" ]
+then
+    if [ ${xterm_pids-none} != "none" ]
+    then
+	kill ${xterm_pids[*]} 
+    fi
+fi
+
+echo $0 Done.
+exit 0
+}
+
+function uptimenow(){
+    cat /proc/uptime | sed -n 's/\([0-9]*\.[0-9]*\) .*/\1/p'
+    
+}
+
+function numdif(){
+    echo $(dc -e "$1 $2 - p")
+}
+
+
+function mydate() {
+ echo $(date +%m-%d-%R) #Shell doesn't like colons
+}
