56062af3a0300bf45854f0d9c5c171bc7a352b71
Seth Chaiken <sdc@cs.albany.edu>
2026-06-09 09:48:15 -0400

    Finished python code to combine images with Phase 1a and any Phase1b data graphically, by expressing the data in .svg (small, vector graphics) files.  Developer convenience..run CT to setup multiple shells without unnecessary sudo for making core dumps in cwds.

---
+++ Graphics/devel/newmagicksetpath.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+PATH=/data/Software/ImageMagick:/data/Software/ImageMagick/bin:$PATH
+MANPATH=/data/Software/ImageMagick/share/man:$MANPATH
+firefox-seth /data/Software/ImageMagick/share/doc/ImageMagick-7/index.html &
+/usr/bin/bash
+echo "Hi. You're getting back into your old shell, you shy clam."
+
+
--- src/ezcores.sh
+++ src/ezcores.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd /proc/sys/kernel
-echo core > core_pattern
-echo 0 > core_uses_pid
+if [ `cat /proc/sys/kernel/core_pattern`baby != corebaby ]
+then
+    cd /proc/sys/kernel
+    if ! sudo echo core > core_pattern
+       then
+	   echo We just use sudo to make core dumps go into cwd, not important.
+       fi
+    sudo echo 0 > core_uses_pid
+fi
+
