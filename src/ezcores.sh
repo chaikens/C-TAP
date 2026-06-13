d87e1a4e6e4aea9edaf943337c595607f97c3db3
Seth Chaiken <sdc@cs.albany.edu>
2026-06-13 08:57:03 -0400

    developer convenience.

--- src/ezcores.sh
+++ src/ezcores.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [ "`cat /proc/sys/kernel/core_pattern`"baby != corebaby ]
-then
     cd /proc/sys/kernel
     if ! sudo echo core > core_pattern
        then
 	   echo We just use sudo to make core dumps go into cwd, not important.
+	   exit 0
        fi
     sudo echo 0 > core_uses_pid
-fi
+
 
