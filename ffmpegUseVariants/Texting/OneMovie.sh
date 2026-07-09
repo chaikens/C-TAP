e11ed680cfd7e3593cefa98c07247fa098f22beb
Seth Chaiken <sdc@cs.albany.edu>
2026-07-09 16:39:14 -0400

    change logging.

--- ffmpegUseVariants/Texting/OneMovie.sh
+++ ffmpegUseVariants/Texting/OneMovie.sh
@@ -2,7 +2,7 @@
 
 FF=ff
 
-FFOPT="-hide_banner -loglevel warning "
+FFOPT="-hide_banner "
 
 srcdir=$1
 movie=$2
@@ -11,9 +11,9 @@ dstdir=$3
 mkdir -p $dstdir
 
 
-${FF}mpeg  -i $srcdir/$movie -an -filter:v 'drawtext=fontfile=arial.ttf:text=%{n}:x=(w-tw)-140:y=(2*lh)-80:fontcolor=white:box=1:boxcolor=0x00000099:fontsize=60,scale=trunc(iw/4)*2:trunc(ih/4)*2,decimate=cycle=5,setpts=N/100/TB' -vsync vfr -y $dstdir/temp.mp4
+${FF}mpeg  -i $srcdir/$movie -an -filter:v 'drawtext=fontfile=arial.ttf:text=%{n}:x=(w-tw)-140:y=(2*lh)-80:fontcolor=white:box=1:boxcolor=0x00000099:fontsize=60,scale=trunc(iw/4)*2:trunc(ih/4)*2,decimate=cycle=5,setpts=N/100/TB' -vsync vfr -y $dstdir/temp.mp4 &>>$movie.log
 
-${FF}mpeg -i $dstdir/temp.mp4 -an -filter:v 'drawtext=fontfile=arial.ttf:text=%{n}:x=(w-tw)-200:y=(2*lh)-40:fontcolor=white:box=1:boxcolor=0x00000099:fontsize=30' -vsync vfr -y $dstdir/$movie
+${FF}mpeg -i $dstdir/temp.mp4 -an -filter:v 'drawtext=fontfile=arial.ttf:text=%{n}:x=(w-tw)-200:y=(2*lh)-40:fontcolor=white:box=1:boxcolor=0x00000099:fontsize=30' -vsync vfr -y $dstdir/$movie &>>$movie.log
 
 
 
