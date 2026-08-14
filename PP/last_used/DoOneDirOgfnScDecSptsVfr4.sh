506c8132f3328ade72d64bf81219279471b162aa
Seth Chaiken <sdc@cs.albany.edu>
2026-08-14 10:27:07 -0400

    Archived preprocessing PP ffmpeg calling scripts used for BENCAM.

---
+++ PP/last_used/DoOneDirOgfnScDecSptsVfr4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PATH=`pwd`:$PATH
+
+DIR=$1  #should be a single token, please run in dir with this subdir.
+DESTDIRDIR=/home/ctap/BENCAM-COPY
+DESTDIR=${DESTDIRDIR}/PP-$1
+
+mkdir -p $DESTDIR
+
+for mov in ${DIR}/N*.mp4  #This pattern excludes the .Nblaaablaa.mp4 files made by bad apples. 
+do
+    mov=$(basename $mov)
+    echo $mov
+    OneMovieOgfnScDecSptsVfr4.sh $DIR $mov $DESTDIR
+done
+
