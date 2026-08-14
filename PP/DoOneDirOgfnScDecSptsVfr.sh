477e10493276d6272813d748e3e13764ab9b2168
Seth Chaiken <sdc@cs.albany.edu>
2026-08-14 10:58:29 -0400

    Documented PP scripts for future experimentation.

--- PP/DoOneDirOgfnScDecSptsVfr.sh
+++ PP/DoOneDirOgfnScDecSptsVfr.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# We called this script in an "at" job as follows:
+#
+# in a directory of daily movie dirs, like 2025-04-28,
+# BE SURE, FOR THE SAKE OF THE AT JOB:
+#
+# PATH=`pwd`:$PATH
+#at <when> (like "now + 700 minutes"
+# DoOneDirOgfnScDecVfr.sh 2025-04-27 > log.427 &
+# DoOneDirOgfnScDecVfr.sh 2025-04-28 > log.428
+# ^D (i.e., <EOF>
+#
+# we had set DESTDIR=/home/ctap/BENCAM-COPY
+#but if you run this script as is, it will create
+#the PP-.. version of the dir. (like 2025-04-27) you
+#call it on.
+
+
 PATH=`pwd`:$PATH
 
 DIR=$1  #should be a single token, please run in dir with this subdir.
